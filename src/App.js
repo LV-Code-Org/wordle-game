100007a51caa5b4cd9b61789f1f448370d6a862d
Vihaan Sinha <chieflordv7@gmail.com>
2023-11-19 19:52:59 -0500

    navbar addition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './css/App.css';
 import Board from "./components/Board"
+import Navbar from './components/Navbar';
 // import Keyboard from './components/Keyboard';
 
 function App() {
   return <center>
+    <Navbar />
     <Board />
-    {/* <Keyboard /> */}
   </center>;
 }
 
